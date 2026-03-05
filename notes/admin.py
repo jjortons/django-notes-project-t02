61a0357808c47d610ecd8e8819e01b7bf6214d5b
jjortons <jjortons@gmail.com>
2026-03-04 19:30:25 -0700

    Changed add_date to created_at in admin.py

--- notes/admin.py
+++ notes/admin.py
@@ -4,6 +4,6 @@ from .models import Note
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
     # This makes the admin list look professional
-    list_display = ('title', 'category', 'note_date', 'date_add')
+    list_display = ('title', 'category', 'note_date', 'created_at')
     list_filter = ('category', 'note_date')
     search_fields = ('title', 'content')
