b6ddf6931eacfdc2f11cee3d02e832838ebd24c0
jjortons <jjortons@gmail.com>
2026-03-04 19:01:48 -0700

    Added views and index template

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Note
 
-# Create your views here.
+def note_list(request):
+    # Fetch all notes, ordered by the date they were taken
+    notes = Note.objects.all().order_by('-note_date')
+    return render(request, 'notes/index.html', {'notes': notes})
